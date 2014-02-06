a9add124160c0adec3942611bbac7d075daaf89f
storm345 <storm345dev@gmail.com>
2014-02-06 20:59:10 +0000

    Finish Autopilot API

--- src/main/java/net/stormdev/uPlanes/api/uPlanesAPI.java
+++ src/main/java/net/stormdev/uPlanes/api/uPlanesAPI.java
@@ -9,6 +9,8 @@ package net.stormdev.uPlanes.api;
 public class uPlanesAPI {
 	private static uPlanesAPI instance;
 	private static uPlaneManager planeManager;
+	private static AutopilotManager autopilotManager;
+	private static final double API_Version = 1.0;
 	
 	/**
 	 * Grab the current instance of the uPlanesAPI so you can do stuff!
@@ -34,6 +36,26 @@ public class uPlanesAPI {
 		return planeManager;
 	}
 	
+	/**
+	 * Contains methods for creating, destroying and manipulating planes
+	 * 
+	 * @return Returns the uPlaneManager instance
+	 */
+	public static AutopilotManager getAutopilotManager(){ //Retrieve instance of PlaneManager
+		if(autopilotManager == null){
+			autopilotManager = new AutopilotManager(); //Work as a factory also
+		}
+		return autopilotManager;
+	}
+	
+	/**
+	 * Get the version of the available API so you can check compatibility
+	 * 
+	 * @return Returns the API Version
+	 */
+	public static double getAPIVersion(){
+		return API_Version;
+	}
 	
 	
 	private uPlanesAPI(){ //Stop other instantating the API
